0eceb9dc9344cce2dcb30f456901d54578ccb41a
balerum03 <adafer123.angra@gmail.com>
2020-08-26 00:23:05 -0500

    added tic-tac-toe_spec.rb file and one more lib called interface.rb

--- spec/tic-tac-toe_spec.rb
+++ spec/tic-tac-toe_spec.rb
@@ -1,9 +1,67 @@
-require_relative '../bin/main'
+require_relative '../lib/interface.rb'
+require_relative '../lib/board.rb'
+require_relative '../lib/players.rb'
 
-decribe Interface
+describe Interface do
   let(:interface){Interface.new}
-  let(:test_array){[1...3]}
+  let(:test_array){%w[1 2 3 4 5 6 7 8 9]}
   it "displays the board" do
-    expect(interface.display_board(test_array)).to eql(test_array)
+    expect(interface.display_board(test_array)).to eql(nil)
+  end
+end
+
+describe Board do
+  let(:board_obj){Board.new(%w[1 2 3 4 5 6 7 8 9])}
+  it "Changes the possition given for 'X' " do
+    expect(board_obj.swap_on_board(2,5)).to eql(board_obj.board)
+  end
+end
+
+describe Board do
+  let(:board_obj){Board.new(%w[1 2 3 4 5 6 7 8 9])}
+  it "Changes the possition given for 'O' " do
+    expect(board_obj.swap_on_board(5,9)).to eql(board_obj.board)
+  end
+end
+
+describe Board do
+  let(:board_obj){Board.new(['X', 'O', 'X', 'O', 'X', 'O', 'X', '8', '9'])}
+    it "Returns True when there is a winner otherwise it returns False" do
+      expect(board_obj.winner?(6)).to eql(true)
+    end
+end
+
+describe Board do
+  let(:board_obj){Board.new(['X', '2', '3', '4', '5', '6', '6', '8', '9'])}
+    it "Returns True when there is a winner otherwise it returns False" do
+      expect(board_obj.winner?(0)).to eql(false)
+    end
+end
+
+describe Board do
+  let(:board_obj){Board.new(['X', 'O', 'X', 'X', 'O', 'X', 'O', 'X', 'O'])}
+    it "Returns true when the array has no integers left on it" do
+      expect(board_obj.draw?).to eql(true)
+    end
+end
+
+describe Board do
+  let(:board_obj){Board.new(['X', 'O', 'X', 'X', 'O', 'X', 'O', 'X', '9'])}
+    it "Returns true when the array has no integers left on it" do
+      expect(board_obj.draw?).to eql(false)
+    end
+end
+
+describe Players do
+  let(:player_name){Players.new('Adan', 'Sam')}
+  context "Returns player's one name when number is even" do
+    it "Returns the name of the player" do
+      expect(player_name.get_name(0)).to eql('Adan')
+    end
+  context "Returns player's two name when number is odd" do
+    it "Returns the name of the player" do
+      expect(player_name.get_name(1)).to eql('Sam')
+    end
+  end
   end
 end
