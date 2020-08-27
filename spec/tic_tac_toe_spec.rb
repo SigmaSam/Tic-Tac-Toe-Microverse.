f78af017767b48cff651082bf3160ebd17035568
balerum03 <adafer123.angra@gmail.com>
2020-08-26 22:50:44 -0500

    Made changes requested by TSE

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -15,40 +15,38 @@ describe Board do
   it 'Changes the possition given for "X" ' do
     expect(board_obj.swap_on_board(2, 5)).to eql(board_obj.board)
   end
-end
 
-describe Board do
-  let(:board_obj) { Board.new(%w[1 2 3 4 5 6 7 8 9]) }
   it 'Changes the possition given for "O" ' do
     expect(board_obj.swap_on_board(5, 9)).to eql(board_obj.board)
   end
-end
+#Negative scenario
+  it 'Does not changes the position when wrong argument is given' do
+    expect(board_obj.swap_on_board(5, 'a')).to eql(board_obj.board)
+  end
 
-describe Board do
-  let(:board_obj) { Board.new(%w[X O X O X O X 8 9]) }
+  let(:board_obj1) { Board.new(%w[X O X O X O X 8 9]) }
   it 'Returns True when there is a winner otherwise it returns False' do
-    expect(board_obj.winner?(6)).to eql(true)
+    expect(board_obj1.winner?(6)).to eql(true)
   end
-end
 
-describe Board do
-  let(:board_obj) { Board.new(%w[X 2 3 4 5 6 6 8 9]) }
+  let(:board_obj2) { Board.new(%w[X 2 3 4 5 6 6 8 9]) }
   it 'Returns True when there is a winner otherwise it returns False' do
-    expect(board_obj.winner?(0)).to eql(false)
+    expect(board_obj2.winner?(0)).to eql(false)
   end
-end
-
-describe Board do
-  let(:board_obj) { Board.new(%w[X O X X O X O X O]) }
-  it 'Returns true when the array has no integers left on it' do
-    expect(board_obj.draw?).to eql(true)
+#Negative scenario were it returns false and there is a winner
+  let(:board_obj5) { Board.new(%w[X O X O X O x 8 9]) }
+  it 'Returns True when there is a winner otherwise it returns False' do
+    expect(board_obj5.winner?(0)).to eql(false)
   end
-end
 
-describe Board do
-  let(:board_obj) { Board.new(%w[X O X X O X O X 9]) }
+  let(:board_obj3) { Board.new(%w[X O X X O X O X O]) }
   it 'Returns true when the array has no integers left on it' do
-    expect(board_obj.draw?).to eql(false)
+    expect(board_obj3.draw?).to eql(true)
+  end
+#Negative scenario base on the elements the array has.
+  let(:board_obj4) { Board.new(%w[X O X X O X O X o]) }
+  it 'Returns false when the array has no integers left on it' do
+    expect(board_obj4.draw?).to eql(false)
   end
 end
 
@@ -62,6 +60,11 @@ describe Players do
       it 'Returns the name of the player' do
         expect(player_name.get_name(1)).to eql('Sam')
       end
+#Negative scenario(concider that the argument is never gonna be more than 9 so this would not happened)
+      it 'Returns false when the argument is not a number' do
+        expect(player_name.get_name(10)).to eql('Adan')
+      end
     end
+
   end
 end
